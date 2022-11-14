bb8996e63d5e41375c320ae0703a1c1877e02b57
nyuta <nyuta.main@gmail.com>
2022-11-14 00:20:28 +0000

    🐛 fix formatter tool

--- tests/test_daily_buy_btc.py
+++ tests/test_daily_buy_btc.py
@@ -1,7 +1,14 @@
-from dotenv import load_dotenv
 import os
 
+from dotenv import load_dotenv
+
+from daily_buy_btc import __version__
+
+load_dotenv()
+
+api_key = os.environ["BITBANK_API_KEY"]
+
+
 def test_version():
-    load_dotenv()
-    api_key = os.environ("API_KEY")
+    assert __version__ == "0.1.0"
     print(api_key)
